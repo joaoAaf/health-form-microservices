ea9436affb78b5dd20eeb7a05b1cc30eb7b580c7
joaoAaf <joao.anderson.af@gmail.com>
2024-05-04 14:52:46 -0300

    Iniciando a criancao do UserController

---
+++ service-users/src/main/java/estudo/serviceusers/controller/UserController.java
@@ -0,0 +1,40 @@
+package estudo.serviceusers.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import estudo.serviceusers.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("user")
+public class UserController extends BaseController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        return getResponse(userService.findUsers(), HttpStatus.OK);
+    }
+    
+    @GetMapping
+    public ResponseEntity<Object> getUser(HttpServletResponse response) {
+        try {
+            var user = userService.findUserId(response.getHeader("X-User-Id"));
+            if (user.isPresent()) {
+                return getResponse(user.get(), HttpStatus.OK);
+            }
+            return getResponse("Este usuário não existe.", HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return getResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+
+
+}
