26606c37023d72cfbbe8ef1bb75f170dd2930193
joaoAaf <joao.anderson.af@gmail.com>
2024-05-10 18:41:59 -0300

    Criando endpoint para deletar usuarios e refatorando demais endpoints

--- service-users/src/main/java/estudo/serviceusers/controller/UserController.java
+++ service-users/src/main/java/estudo/serviceusers/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +27,13 @@ public class UserController extends BaseController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<Object> registerUser(@RequestBody @Valid UserSave user, HttpServletRequest request) {
+    public ResponseEntity<Object> registerUser(@RequestBody @Valid UserSave userSave, HttpServletRequest request) {
         try {
-            if (userService.findUserEmail(user.email()).isEmpty()) {
+            if (userService.findUserEmail(userSave.email()).isEmpty()) {
+                var user = userService.saveUser(userSave);
                 log(this, request.getMethod(), request.getRequestURI(), "Novo id cadastrado",
                         HttpStatus.CREATED.value());
-                return getResponse(userService.saveUser(user), HttpStatus.CREATED);
+                return getResponse(user, HttpStatus.CREATED);
             }
             log(this, request.getMethod(), request.getRequestURI(), "Não cadastrado",
                     HttpStatus.BAD_REQUEST.value(), "Já existe um usuário com este email");
@@ -68,9 +70,30 @@ public class UserController extends BaseController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         try {
             if (userService.findUserId(authentication.getName()).isPresent()) {
+                var user = userService.updateUser(newUser, authentication.getName());
                 log(this, request.getMethod(), request.getRequestURI(), authentication.getName(),
                         HttpStatus.OK.value());
-                return getResponse(userService.updateUser(newUser, authentication.getName()), HttpStatus.OK);
+                return getResponse(user, HttpStatus.OK);
+            }
+            log(this, request.getMethod(), request.getRequestURI(), authentication.getName(),
+                    HttpStatus.OK.value(), "Este usuário não existe");
+            return getResponse("Este usuário não existe", HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log(this, request.getMethod(), request.getRequestURI(), authentication.getName(),
+                    HttpStatus.OK.value(), e.getMessage());
+            return getResponse("Serviço indisponível", HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> deleteUser(HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        try {
+            if (userService.findUserId(authentication.getName()).isPresent()) {
+                userService.deleteUser(authentication.getName());
+                log(this, request.getMethod(), request.getRequestURI(), authentication.getName(),
+                        HttpStatus.NO_CONTENT.value());
+                return getResponse(null, HttpStatus.NO_CONTENT);
             }
             log(this, request.getMethod(), request.getRequestURI(), authentication.getName(),
                     HttpStatus.OK.value(), "Este usuário não existe");
