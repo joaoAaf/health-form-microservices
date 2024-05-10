ac7b23687c02cfbfc7381ad8f58bf5f3e99cf35e
joaoAaf <joao.anderson.af@gmail.com>
2024-05-10 19:07:28 -0300

    Criando service-imc

---
+++ service-imc/src/test/java/estudo/serviceimc/ServiceImcApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.serviceimc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceImcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
