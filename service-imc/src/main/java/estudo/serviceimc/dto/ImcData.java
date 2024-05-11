2aa5e1f833d30363caba738af748dd3f83aa1040
joaoAaf <joao.anderson.af@gmail.com>
2024-05-11 16:08:37 -0300

    Incluindo msgs de validação dos dados no ImcData e refatorando a classe Imc

--- service-imc/src/main/java/estudo/serviceimc/dto/ImcData.java
+++ service-imc/src/main/java/estudo/serviceimc/dto/ImcData.java
@@ -3,6 +3,12 @@ package estudo.serviceimc.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
-public record ImcData(@NotBlank @Positive double weight, @NotBlank @Positive double height) {
+public record ImcData(
+    @NotBlank(message = "'weight' deve ser preenchido") 
+    @Positive(message = "'weight' deve maior que '0'")
+    double weight, 
+    @NotBlank(message = "'height' deve ser preenchido")
+    @Positive(message = "'height' deve maior que '0'")
+    double height) {
 
 }
--- service-imc/src/main/java/estudo/serviceimc/model/Imc.java
+++ service-imc/src/main/java/estudo/serviceimc/model/Imc.java
@@ -7,16 +7,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Imc {
 
-    private double weight;
-    private double height;
     private double imc;
     private String msg;
     private String diet;
 
     public Imc(double weight, double height) {
         var imc = calcImc(weight, height);
-        this.weight = weight;
-        this.height = height;
         this.imc = imc;
         defineDiet(imc);
     }
@@ -26,20 +22,19 @@ public class Imc {
     }
 
     private void defineDiet(double imc) {
-    if (imc < 18.5) {
-        setMsg("Você está abaixo do peso. Tente consumir alimentos ricos em proteínas e calorias para ganhar peso.");
-        setDiet("Menu recomendado: Café da manhã: Omelete com vegetais; Almoço: Frango grelhado com arroz integral e legumes; Jantar: Salmão assado com batata doce e brócolis.");
-    } else if (imc <= 24.9) {
-        setMsg("Você está com peso normal. Continue com uma dieta equilibrada e pratique exercícios regularmente.");
-        setDiet("Menu recomendado: Café da manhã: Aveia com frutas e iogurte; Almoço: Salada verde com frango grelhado; Jantar: Peixe cozido com quinoa e legumes.");
-    } else if (imc <= 29.9) {
-        setMsg("Você está com sobrepeso. Reduza a ingestão de calorias e faça exercícios regularmente.");
-        setDiet("Menu recomendado: Café da manhã: Smoothie de frutas e vegetais; Almoço: Sanduíche de peru e salada; Jantar: Espaguete integral com molho de tomate caseiro.");
-    } else {
-        setMsg("Você está obeso. Consulte um nutricionista para um plano de dieta personalizado.");
-        setDiet("Menu recomendado: Café da manhã: Omelete de claras com espinafre; Almoço: Salada de grãos com frango grelhado; Jantar: Sopa de legumes com carne magra.");
+        if (imc < 18.5) {
+            setMsg("Você está abaixo do peso. Tente consumir alimentos ricos em proteínas e calorias para ganhar peso.");
+            setDiet("Menu recomendado: Café da manhã: Omelete com vegetais; Almoço: Frango grelhado com arroz integral e legumes; Jantar: Salmão assado com batata doce e brócolis.");
+        } else if (imc <= 24.9) {
+            setMsg("Você está com peso normal. Continue com uma dieta equilibrada e pratique exercícios regularmente.");
+            setDiet("Menu recomendado: Café da manhã: Aveia com frutas e iogurte; Almoço: Salada verde com frango grelhado; Jantar: Peixe cozido com quinoa e legumes.");
+        } else if (imc <= 29.9) {
+            setMsg("Você está com sobrepeso. Reduza a ingestão de calorias e faça exercícios regularmente.");
+            setDiet("Menu recomendado: Café da manhã: Smoothie de frutas e vegetais; Almoço: Sanduíche de peru e salada; Jantar: Espaguete integral com molho de tomate caseiro.");
+        } else {
+            setMsg("Você está obeso. Consulte um nutricionista para um plano de dieta personalizado.");
+            setDiet("Menu recomendado: Café da manhã: Omelete de claras com espinafre; Almoço: Salada de grãos com frango grelhado; Jantar: Sopa de legumes com carne magra.");
+        }
     }
-}
-
 
 }
