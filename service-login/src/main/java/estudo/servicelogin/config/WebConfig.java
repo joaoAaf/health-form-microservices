5b0740f917ebd8f6cc05b18555f79faf268120aa
joaoAaf <joao.anderson.af@gmail.com>
2024-05-13 21:53:02 -0300

    Configurando o cors no service-login

---
+++ service-login/src/main/java/estudo/servicelogin/config/WebConfig.java
@@ -0,0 +1,18 @@
+package estudo.servicelogin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST")
+                .maxAge(3600);
+    }
+
+}
