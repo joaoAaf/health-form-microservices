f12f69c9761a302542526525154336178991d32a
joaoAaf <joao.anderson.af@gmail.com>
2024-05-14 17:22:13 -0300

    Criando variaveis de ambiente

--- service-imc/src/main/java/estudo/serviceimc/service/TokenService.java
+++ service-imc/src/main/java/estudo/serviceimc/service/TokenService.java
@@ -2,6 +2,7 @@ package estudo.serviceimc.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -9,10 +10,13 @@ import com.auth0.jwt.algorithms.Algorithm;
 
 @Service
 public class TokenService {
+
+    @Value("${env-variables.jwtSecret}")
+    private String secret;
     
     public Optional<String> loggedId(String token) {
         try {
-            return Optional.of(JWT.require(Algorithm.HMAC256("essaéapalavrasecreta"))
+            return Optional.of(JWT.require(Algorithm.HMAC256(secret))
             .build().verify(token).getSubject());
         } catch (Exception e) {
             return Optional.empty();
--- service-login/src/main/java/estudo/servicelogin/service/TokenService.java
+++ service-login/src/main/java/estudo/servicelogin/service/TokenService.java
@@ -3,6 +3,7 @@ package estudo.servicelogin.service;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -13,13 +14,16 @@ import estudo.servicelogin.dto.User;
 @Service
 public class TokenService {
 
+    @Value("${env-variables.jwtSecret}")
+    private String secret;
+
     public String generateToken(User user) {
         return JWT.create()
                 .withSubject(user.id())
                 .withExpiresAt(LocalDateTime.now()
                         .plusMinutes(30)
                         .toInstant(ZoneOffset.of("-03:00")))
-                .sign(Algorithm.HMAC256("essaéapalavrasecreta"));
+                .sign(Algorithm.HMAC256(secret));
     }
 
 }
